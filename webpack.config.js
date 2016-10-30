8091b3ddfdfca7d750980ed2c5c0c78320aafadb
Igor Pavlov <gophan1992@gmail.com>
2016-10-30 13:43:30 +0300

    added react-hot-reloader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-
 module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
@@ -8,25 +7,21 @@ module.exports = {
 		'babel-polyfill',
 		'./src/index'
 	], output: {
-		path: path.join(__dirname, 'dist'),
+		path: path.join (__dirname, 'dist'),
 		filename: 'bundle.js',
 		publicPath: '/static/'
 	}, plugins: [
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	],
-	module: {
+		new webpack.optimize.OccurenceOrderPlugin (),
+		new webpack.HotModuleReplacementPlugin (),
+		new webpack.NoErrorsPlugin ()
+	], module: {
 		loaders: [
 			{
-				loaders: ['babel-loader'],
-				include: [
-					path.resolve(__dirname, "src"),
-					path.resolve(__dirname, "src", "containers"),
-				],
+				loaders: [ 'react-hot', 'babel-loader' ],
+				include: [ path.resolve (__dirname, "src") ],
 				test: /\.js$/,
-				plugins: ['transform-runtime'],
+				plugins: [ 'transform-runtime' ],
 			}
-		] }
+		]
+	}
 };
- 
